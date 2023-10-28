881dc37f1faa2f017e5141cce9551fb4ea9a52ab
decidev22 <hamento22@gmail.com>
2023-10-29 00:07:35 +1100

    first commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+const FilteredEventPage = () => {
+  return (
+    <div>
+      <h1>Filtered Event Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEventsPage = () => {
+  return (
+    <div>
+      <g1>All Events</g1>
+    </div>
+  );
+};
+
+export default AllEventsPage;
