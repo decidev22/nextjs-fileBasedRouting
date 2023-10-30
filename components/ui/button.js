fdb51bcfd0c7ec9e4e832fd3a77dc423f80dc392
decidev22 <hamento22@gmail.com>
2023-10-30 20:59:31 +1100

    codemod update to nextjs link

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,8 +3,8 @@ import classes from "./button.module.css";
 
 const Button = (props) => {
   return (
-    <Link href={props.link}>
-      <a className={classes.btn}>{props.children}</a>
+    <Link href={props.link} className={classes.btn}>
+      {props.children}
     </Link>
   );
 };
