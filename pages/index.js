ebf9619ac5c5c73f9e6142a001f386c15c74ce6e
decidev22 <hamento22@gmail.com>
2023-10-30 20:57:59 +1100

    commit prior to NextJS Link <a> update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+
+import EventList from "../components/events/event-list";
+
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>HomePage</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 };
