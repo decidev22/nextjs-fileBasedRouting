8ef8e363642cf6824a4dc0ecd6f6123a5476b4ad
decidev22 <hamento22@gmail.com>
2023-11-24 14:42:45 +1100

    imported event-detail

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Button from "../ui/button";
 import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -22,14 +25,21 @@ const EventItem = (props) => {
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
-      </div>
-      <div className={classes.actions}>
-        <Button link={exploreLink}>Explore Event</Button>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
       </div>
     </li>
   );
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event was found!</p>;
+  }
+
   return (
     <div>
       <h1>Event Details</h1>
